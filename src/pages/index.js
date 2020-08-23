7cb15fab7065c350e47c9e58885a3ee27f159b54
Jir Wong <jirwong@gmail.com>
2020-08-23 10:51:47 +1000

    Added tailwind css configuration.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React from "react";
+
+import "../../css/tailwind.css";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Hello world!</div>;
 }
